4b6d6c8f5833fee4001caa182f738eba882a194f
Caennew <121186825+Caennew@users.noreply.github.com>
2023-02-21 22:10:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from rectangle import Restangle,square, circle
+
+rect_1 = Restangle(3,4)
+rect_2 = Restangle(12,5)
+
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(15)
+
+print(square_1.get_area(),square_2.get_area())
+
+circle_1 = Сircle(4)
+circle_2 = Сircle(12)
+
+print(circle_1.get_area_circle(),circle_2.get_area_circle())
